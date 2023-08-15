2d85de6918d011a697ce04674e03892f4de183e5
for shell <fromShell@paas.com>
2023-08-15 11:05:18 +0800

    test

--- main.cpp
+++ main.cpp
@@ -1,24 +1,44 @@
 #include"httplib/httpserver.h"
 
 
+
+
+
 #include <iostream>
+
 using namespace std;
- 
-
-
-
 
  
+
+
+
+
+
+
+ 
+
 int main()
+
 {
+
     EventLoop loop;
+
     InetAddress listenaddr(8888);
+
     // HttpServer(EventLoop *loop, const InetAddress & linten_addr, 
+
     //           const string & name, TcpServer::Option option = TcpServer::kNoReusePort);
+
     HttpServer server(&loop, listenaddr, "default_http");
-    cout << "created the server"; 
+
+    cout << "created the server"<< endl; 
+
     server.start();
+
     loop.loop();
+
     
+
     return 0;
+
 }
